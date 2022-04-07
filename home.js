6a4b71de4101490c869f5bfb8665c51a47b5587f
Calci cool <31652458+Mithilesh27@users.noreply.github.com>
2022-04-07 15:03:07 +0530

    UI update

---
+++ home.js
@@ -0,0 +1,42 @@
+const quizdb = [
+    {
+        question: "Q1. What is the full form of HTML",
+        a: "Hello To My Land",
+        b: "Hey Text Markup Language",
+        c: "Hyper Text Makeup Language",
+        d: "Hyper Text Markup Language",
+        ans: "ans4"
+    },
+    {
+        question: "Q2. Which HTML tag is used to define an internal style sheet?",
+        a: "<script>",
+        b: "<css>",
+        c: "<style>",
+        d: " None of these",
+        ans: "ans3"
+    },
+    {
+        question: "Q3. Using _______ statement is how you test for a specific condition.",
+        a: "select",
+        b: "if",
+        c: "switch",
+        d: "for",
+        ans: "ans2"
+    },
+    {
+        question: "Q4. Who is making the Web standards?",
+        a: "Mozilla",
+        b: "Microsoft",
+        c: "The World Wide Web Consortium",
+        d: "Nvidia",
+        ans: "ans2"
+    },
+    {
+        question: "Q5. Choose the correct HTML tag for the largest heading",
+        a: "<h6>",
+        b: "<heading>",
+        c: "<head>",
+        d: "<h1>",
+        ans: "ans4"
+    }
+]
